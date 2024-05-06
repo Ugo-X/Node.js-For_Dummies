e879a87d007ea8d84b93c3b533bc626b6df0231e
Ugo-X <ugonnadike001@gmail.com>
2024-05-06 03:39:05 +0100

    updated Node Day one note

--- Day-1/Day-1.js
+++ Day-1/Day-1.js
@@ -1,6 +1,13 @@
-function findMeaning (){
-    return 42
-}
-console.log(findMeaning());
+// How to use promises in a real world situation
+// const urls = [
+//   "https://jsonplaceholder.typicode.com/users",
+//   "https://jsonplaceholder.typicode.com/albums",
+//   "https://jsonplaceholder.typicode.com/posts",
+// ];
 
+// Promise.all(urls.map((url)=>{
+// return fetch(url).then((reponse) => reponse.json()).then((data)=>{
+//     console.log(data);
+// })
+// }))
 

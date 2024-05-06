3d0b1921a2ddc934aff797d0d641b37dc78e8eef
Ugo-X <ugonnadike001@gmail.com>
2024-05-06 17:43:03 +0100

    updated day one note

--- Day-1/Day-1.js
+++ Day-1/Day-1.js
@@ -11,7 +11,7 @@ const urls = [
 // })
 // }))
 
-// USING ASYNC/AWAIT 
+// USING ASYNC/AWAIT
 // async function fetchUsers() {
 //   const response = await fetch(urls[0]);
 //   const data = await response.json();
@@ -23,28 +23,27 @@ const urls = [
 
 // REAL LIFE EXAMPLE OF ASYNC/AWAIT/finally
 
-async function getData(){
-  try {
-    const [users, albums, posts] = await Promise.all(urls.map(async (url) => {
-        const response = await fetch(url)
-        if(!response.ok){
-            throw new Error(`HTTP error! status: ${response.status}`)
-        }
-     const data = await response.json()
-  return data
-      }))
-      console.log(users, albums,posts);
-  } catch (error) {
-    console.log("error", error.message);
-  } 
-//   finally will run regardless of if the code above is fulfilled or rejected
-  finally{
-    console.log('the world is green when the sun is high in the sky');
-  }
-}
+// async function getData(){
+//   try {
+//     const [users, albums, posts] = await Promise.all(urls.map(async (url) => {
+//         const response = await fetch(url)
+//         if(!response.ok){
+//             throw new Error(`HTTP error! status: ${response.status}`)
+//         }
+//      const data = await response.json()
+//   return data
+//       }))
+//       console.log(users, albums,posts);
+//   } catch (error) {
+//     console.log("error", error.message);
+//   }
+// //   finally will run regardless of if the code above is fulfilled or rejected
+//   finally{
+//     console.log('the world is green when the sun is high in the sky');
+//   }
+// }
 
-getData()
-
+// getData()
 
 // const loopThroughUrls = url =>{
 //     for (const url of urls) {
@@ -55,12 +54,58 @@ getData()
 // loopThroughUrls()
 
 // using for await of loop
-const getData2 = async()=>{
-    const arrayOfPromises = urls.map( async url => await fetch(url))
-    for await (let request of arrayOfPromises){
-        console.log(request);
-    }
+// const getData2 = async () => {
+//   const arrayOfPromises = urls.map(async (url) => await fetch(url));
+//   for await (let response of arrayOfPromises) {
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
+//   }
+// };
 
-}
+// getData2();
 
-getData2()
+// # # # # # # # # # #
+// when dealing with promises, there are three ways to run them
+// 1 parallel which runs them all at the same time
+
+// 2 sequential which runs each promise or call in order
+// 3 race (call three things but whichever comes back first, do that one and ignore the rest )
+
+const promisify = (item, delay) =>
+  new Promise((resolve) => setTimeout(() => resolve(item), delay));
+const a = () => promisify("a", 1000);
+const b = () => promisify("b", 2000);
+const c = () => promisify("c", 3000);
+
+// The parallel way of running
+const parallel = async () => {
+  const promises = [a(), b(), c()];
+  const [output1, output2, output3] = await Promise.all(promises);
+  return `parallel is done running: ${output1} ${output2} ${output3}`;
+};
+
+parallel().then((data) => console.log(data));
+
+// ### The sequential way of running it
+
+const sequential = async () => {
+  const output1 = await a();
+  const output2 = await b();
+  const output3 = await c();
+  return `sequential is done running: ${output1} ${output2} ${output3}`;
+};
+
+sequential().then((data) => console.log(data));
+
+// the race method of running it
+// note that this method runs one and ignores the rest of the promises
+const race = async () => {
+  const promises = [a(), b(), c()];
+  const output1 = await Promise.race(promises);
+  return `The race is over and the winner is ${output1}`
+};
+
+race().then((data) => console.log(data));
+
+// promise.allSettled runs all the promises regardless of if they are fulfiled or rejected. when they return, you will se which one was rejected or settled.

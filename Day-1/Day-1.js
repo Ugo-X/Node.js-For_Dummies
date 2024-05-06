dd9776f39ee42bced10813e504a030d375faa907
Ugo-X <ugonnadike001@gmail.com>
2024-05-06 11:42:24 +0100

    update to note

--- Day-1/Day-1.js
+++ Day-1/Day-1.js
@@ -1,9 +1,9 @@
 // How to use promises in a real world situation
-// const urls = [
-//   "https://jsonplaceholder.typicode.com/users",
-//   "https://jsonplaceholder.typicode.com/albums",
-//   "https://jsonplaceholder.typicode.com/posts",
-// ];
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/albums",
+  "https://jsonplaceholder.typicode.com/posts",
+];
 
 // Promise.all(urls.map((url)=>{
 // return fetch(url).then((reponse) => reponse.json()).then((data)=>{
@@ -11,3 +11,56 @@
 // })
 // }))
 
+// USING ASYNC/AWAIT 
+// async function fetchUsers() {
+//   const response = await fetch(urls[0]);
+//   const data = await response.json();
+//   console.log(data[0],data[1]);
+// //   return data[0]
+// }
+
+// fetchUsers();
+
+// REAL LIFE EXAMPLE OF ASYNC/AWAIT/finally
+
+async function getData(){
+  try {
+    const [users, albums, posts] = await Promise.all(urls.map(async (url) => {
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+     const data = await response.json()
+  return data
+      }))
+      console.log(users, albums,posts);
+  } catch (error) {
+    console.log("error", error.message);
+  } 
+//   finally will run regardless of if the code above is fulfilled or rejected
+  finally{
+    console.log('the world is green when the sun is high in the sky');
+  }
+}
+
+getData()
+
+
+// const loopThroughUrls = url =>{
+//     for (const url of urls) {
+//         console.log(url);
+//     }
+// }
+
+// loopThroughUrls()
+
+// using for await of loop
+const getData2 = async()=>{
+    const arrayOfPromises = urls.map( async url => await fetch(url))
+    for await (let request of arrayOfPromises){
+        console.log(request);
+    }
+
+}
+
+getData2()

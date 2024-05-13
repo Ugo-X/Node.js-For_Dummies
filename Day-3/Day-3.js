488838aa3783a675ed9f014e71944f04acb15013
Ugo-X <ugonnadike001@gmail.com>
2024-05-13 23:12:39 +0100

    git update

--- Day-3/Day-3.js
+++ Day-3/Day-3.js
@@ -1,3 +1,8 @@
 // CREATING OUR OWN MODULES
+// refer to https.js/response and request.js
+
+// Caching in Node
+// to prevent code/command that are part of a module to be run multiple TimeRanges, it maintains a cache. This way even if the module is required multiple times, the code in the module will run only once.
+
 
 

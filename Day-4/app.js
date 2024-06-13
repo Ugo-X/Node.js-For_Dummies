a43e87f405dc6e062bded68c7a0761b2d0630227
Ugo-X <ugonnadike001@gmail.com>
2024-06-13 09:03:43 +0100

    updated files

--- Day-4/app.js
+++
@@ -1,5 +0,0 @@
-// NPM
-// NODE PACKAGE MANAGER
-// Packages are these bundles of code that we can reuse, created by numerous developers and shared to sweeten the developement process for others 
-// NPM is used to create and install these pa ckages. npm is resident in node 
-A package is a collection of modules
--- Day-4/modules/http.js
+++ Day-4/modules/http.js
@@ -11,3 +11,11 @@ console.log(requestedData);
 
 
 console.log(require.cache);
+
+
+
+// NPM
+// NODE PACKAGE MANAGER
+// Packages are these bundles of code that we can reuse, created by numerous developers and shared to sweeten the developement process for others 
+// NPM is used to create and install these pa ckages. npm is resident in node 
+A package is a collection of modules

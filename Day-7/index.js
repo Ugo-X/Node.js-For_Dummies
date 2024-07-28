25ec05f525290f3d365d31fabcce7a5c5a5c2388
Ugo-X <ugonnadike001@gmail.com>
2024-07-28 11:42:00 +0100

    Building our own web Sever

---
+++ Day-7/index.js
@@ -0,0 +1,3 @@
+// WHAT IS A WEB SERVER?
+// When we input a website we want to navigate to, the browser calls the Domain Name System that in turn searches the internet and returns an ip(internet protocol) address of the website or server we were trying to locate, which is usually a set of 4 number pairs between zero and 255 seperated by periods. This ip address is what we then use to communicate with the server which will give us the data that we have requested. to specify which app on a server should handle our request, we add a port number eg 12.34.23.56:80
+
